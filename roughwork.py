342aef9aa2e525292ad71ce6e11851eb89c49495
nitesh <nitesh.passau@gmail.com>
2023-01-06 15:14:54 +0100

    1

---
+++ roughwork.py
@@ -0,0 +1,3 @@
+f = open("frameInfo.txt", "w")
+f.write('nitesh\nagrawal')
+f.close()
---
+++ test.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+import webbrowser
+
+filePath = 'frames\\frame8.jpg'
+searchUrl = 'http://www.google.com/searchbyimage/upload'
+multipart = {'encoded_image': (filePath, open(filePath, 'rb')), 'image_content': ''}
+response = requests.post(searchUrl, files=multipart, allow_redirects=False)
+fetchUrl = response.headers['Location']
